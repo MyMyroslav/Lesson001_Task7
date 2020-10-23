945949a7e217fbe6f820a96ac4ed9b6316f6819c
Myroslav <havrushkomyroslav@gmail.com>
2020-10-23 13:07:13 +0300

    Done-Lesson001_Task7

---
+++ Lesson001_Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson001_Task7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            char gender = 'M';
+            Console.WriteLine($"Gender: {gender}");
+        }
+    }
+}
